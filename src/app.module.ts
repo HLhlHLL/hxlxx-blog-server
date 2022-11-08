ba1da5b7c2bd253fa23ff79822cc3c0ca899cbcc
000 <10037022+hlzzxx@user.noreply.gitee.com>
2022-11-08 13:06:48 +0800

    新增user模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ArticleModule } from './article/article.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserModule } from './user/user.module'
 
 @Module({
   imports: [
@@ -18,7 +19,8 @@ import { TypeOrmModule } from '@nestjs/typeorm'
       retryDelay: 500, //重试连接数据库间隔
       retryAttempts: 10, //重试连接数据库的次数
       autoLoadEntities: true //如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
-    })
+    }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService]
